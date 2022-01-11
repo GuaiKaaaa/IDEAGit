e32cf31618bb18cac3c8a09d135bd848880e2c16
GuaiKaaaa <471542104@qq.com>
2022-01-11 23:38:49 +0800

    添加文件

--- HelloGit/src/main/java/com/guaika/git/Hello.java
+++ HelloGit/src/main/java/com/guaika/git/Hello.java
@@ -7,5 +7,7 @@ package com.guaika.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!!!");
+        System.out.println("更新!!!!!");
+
     }
 }
