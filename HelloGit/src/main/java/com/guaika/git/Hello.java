259ce0dae07eb22c2e5e5e899ddde59f7973ea1c
GuaiKaaaa <471542104@qq.com>
2022-01-11 23:35:40 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/guaika/git/Hello.java
@@ -0,0 +1,11 @@
+package com.guaika.git;
+
+/**
+ * @author Guaika
+ * @create 2022-01-11 23:23
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!!!");
+    }
+}
